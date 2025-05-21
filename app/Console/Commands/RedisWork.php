edf3b6a43cc730de12628d9d1f5a5ca64fbcdc03
Supernova <121464151+Hiddengemx@users.noreply.github.com>
2025-05-21 19:49:57 +0300

    Update RedisWork.php

--- app/Console/Commands/RedisWork.php
+++ app/Console/Commands/RedisWork.php
@@ -27,43 +27,7 @@ class RedisWork extends Command
      * Execute the console command.
      */
     public function handle()
-    {
-        // $str = 'Hello world';
-        // $result = '';
-
-        // if(Cache::has('greeting')) {
-        //     $result = Cache::get('greeting');
-        // } else {
-        //     Cache::put('greeting', $str);
-        //     $result = $str;
-        // }
-
-        // $str = "Hello world";
-        // $result = '';
-        // $result = Cache::rememberForever('greeting', function () use($str) {
-        //     return $str;
-        // });
-
-        // dd($result);
-
-
-        // Redis::set('greeting', 'Hello world!');
-        // $greeting = Redis::get('greeting');
-        // dd($greeting);
-
-
-        // $post = Post::find(1);
-        // Redis::set('posts:' . $post->id, $post);
-        // $post = Post::make((array)json_decode($post));
-        // dd($post);
-
-
-        // Redis::lpush('workers', 'Michael', 'Alex', "Mary");
-        
-        // $workers = Redis::lrange('workers', 0, -1);
-        // dd($workers);
-
-                
+    {   
         Post::all()->each(function ($post) {
             Cache::put('posts:' . $post->id, $post);
         });
